fa529cc82b2335adc7a8c1a026cb089e75d87e7d
Vioreed <predimolanaz23@gmail.com>
2023-09-06 14:21:14 +0700

    feat: change config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 export const JWT_SECRET_KEY = 'secret-key-reed';
 export const ALGORITHM = 'HS256';
-export const FILE_URL = 'http://localhost:8002/storage/';
-// export const FILE_URL = 'http://103.63.25.25/storage/';
+export const FILE_URL = 'http://localhost:5000/storage/';
 export const FILE_PATH = '/home/reed/htdocs/matursoft/products/wa-akhyar/storage/';
 export const KEY = 'Matursoft';
 export const WA_ENGINE = 'https://wa-engine.matursoft.com/'
