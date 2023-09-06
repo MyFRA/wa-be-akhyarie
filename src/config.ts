0d88b4545730ba7165367c78bc3df93decb8dfa6
Vioreed <predimolanaz23@gmail.com>
2023-09-06 14:22:46 +0700

    feat: modify gitignore

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const JWT_SECRET_KEY = 'secret-key-reed';
-export const ALGORITHM = 'HS256';
-export const FILE_URL = 'http://localhost:5000/storage/';
-export const FILE_PATH = '/home/reed/htdocs/matursoft/products/wa-akhyar/storage/';
-export const KEY = 'Matursoft';
-export const WA_ENGINE = 'https://wa-engine.matursoft.com/'
